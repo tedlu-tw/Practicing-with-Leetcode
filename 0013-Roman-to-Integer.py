032033546c4b925bb44a4383aca1b7a7d624e211
Ted Lu <49829569+Sax-Ted@users.noreply.github.com>
2021-02-13 11:11:20 +0800

    0013-Roman-to-Integer

---
+++ 0013-Roman-to-Integer.py
@@ -0,0 +1,12 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        s_dict = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+        res, pre = 0, 0
+        for i in s:
+            cur = s_dict[i]
+            if pre < cur:
+                res += (cur - 2 * pre)
+            else:
+                res += cur
+            pre = cur
+        return res
