d0ce52aadb60e773bade7c7611c103ddb0aff6df
Ted Lu <49829569+Sax-Ted@users.noreply.github.com>
2021-02-13 11:46:19 +0800

    0014-Create-Common-Prefix

---
+++ 0014-Longest-Common_Prefix.py
@@ -0,0 +1,10 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        prefix=[]
+        num = len(strs)
+        for x in zip(*strs):
+            if len(set(x)) == 1:
+                prefix.append(x[0])
+            else:
+                break
+        return "".join(prefix)
